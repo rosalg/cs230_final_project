2fac8296819a599e469ec9acdb6855fe39632136
rosalg <rosalg@stanford.edu>
2022-11-10 23:11:31 -0800

    Preprocess pt 1: aggregating A w P

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ def preprocess(data_folder_path):
         path_to_raw_image = os.path.join(raw_path, filename)
         images = os.listdir(path_to_raw_image)
         if len(images) >= 2:
+            processed_img_path = os.path.join(processed_path, filename)
+            os.makedirs(processed_img_path, exist_ok=True)
             for img_file_name in images:
-                img_path = os.path.join(processed_path, filename, img_file_name)
-                img = open(img_path)
-                with open(processed_path, 'r'):
-            # with open('.\data_folder\lfw_processed', "w") as f:
+                raw_image = open(path_to_raw_image)
+                new_processed_img = open(os.path.join(processed_img_path, img_file_name), "wb")
+                new_processed_img.write(raw_image)
+                new_processed_img.close()
 
 
 if __name__ == "__main__":
